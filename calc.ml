159a43ca8b47ba3998141ade45dfdaa299063728
koallen <weifengzi2009@gmail.com>
2016-08-26 00:38:31 +0800

    make calc accept both regex & string

--- calc.ml
+++ calc.ml
@@ -5,13 +5,14 @@
                if String.length w == 0
                then nullable r
                else matcher (simp (deriv r w.[0])) (String.sub w 1 ((String.length w) - 1))
-               
+
         let _ =
           try
             let lexbuf = Lexing.from_channel stdin in
             while true do
-              let result = Parser.main Lexer.token lexbuf in
-                Printf.printf "%b" (nullable result); print_newline(); flush stdout
+              let regex = Parser.main Lexer.token lexbuf in
+              let str = input_line stdin in
+                Printf.printf "%b" (matcher regex str); print_newline(); flush stdout
             done
           with Lexer.Eof ->
             exit 0
