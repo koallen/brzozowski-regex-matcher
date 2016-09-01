7dc47cda9fb0014b3ceffa2273c12f3e6d8c1aee
koallen <weifengzi2009@gmail.com>
2016-09-01 18:35:46 +0800

    reformatting

--- calc.ml
+++ calc.ml
@@ -1,18 +1,18 @@
 (* File calc.ml *)
-        open Re
+open Re
 
-        let rec matcher r w =
-               if String.length w == 0
-               then nullable r
-               else matcher (simp (deriv r w.[0])) (String.sub w 1 ((String.length w) - 1))
+let rec matcher r w =
+       if String.length w == 0
+       then nullable r
+       else matcher (simp (deriv r w.[0])) (String.sub w 1 ((String.length w) - 1))
 
-        let _ =
-          try
-            let lexbuf = Lexing.from_channel stdin in
-            while true do
-              let regex = Parser.main Lexer.token lexbuf in
-              let str = input_line stdin in
-                Printf.printf "%b" (matcher regex str); print_newline(); flush stdout
-            done
-          with Lexer.Eof ->
-            exit 0
+let _ =
+  try
+    let lexbuf = Lexing.from_channel stdin in
+    while true do
+      let regex = Parser.main Lexer.token lexbuf in
+      let str = input_line stdin in
+        Printf.printf "%b" (matcher regex str); print_newline(); flush stdout
+    done
+  with Lexer.Eof ->
+    exit 0
